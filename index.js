c8ed8b8dfe44fa393068503fcafa07be0b622113
Syedmoeez <84286252+Syedmoeez@users.noreply.github.com>
2021-05-16 13:55:56 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// This command used to print
+// Google
+console.log("Welcome to js")
+// Variable
+//var name = 'Moeez'
+//console.log(name)
+
+//var age = 12;
+//console.log(age)
+
+// var firstname = 'Moeez';
+// var lastname = 'ahmed';
+// var space = ' ';// for creating space
+
+// console.log(firstname+space+lastname)// for professionals
+// console.log(firstname+' '+lastname+space+ 'My Age is =' + age)// for beginners
+// var age = prompt("What is your age?")
+// document.getElementById('myage').innerHTML=age;
